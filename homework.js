16a7e9aeec4a9fc47d2d32c84a31e55c2a7bdc2b
alex <appofeoz00@gmail.com>
2019-11-21 01:22:35 +0200

    fix: swapNum function; rewrite: getSum, divNum function; other changes.

--- homework.js
+++ homework.js
@@ -1,30 +1,39 @@
-let array = [1,2,3,80,5,6,14,18,21];
+let array = [15,2,3,1,14,12,21];
 
 //first task
 function calcSum(arr) {
-    let sum = 0;
-    for (i=0;i<arr.length; i++){
-        sum += arr[i];
-    }
-    console.log(sum);
+    let result = arr.reduce(function(sum, current){
+    	return sum + current;
+	});
+    console.log(result);
 }
 
-function getNum(arr){
+function maxNum(arr){
 	console.log(Math.max.apply(null, arr));
+}
+
+function minNum(arr){
 	console.log(Math.min.apply(null, arr));
 }
 
 function swapNum(arr){
-	let temp = arr[arr.indexOf(Math.max.apply(null, arr),0)];
-	arr[arr.indexOf(Math.max.apply(null, arr),0)] = arr[arr.indexOf(Math.min.apply(null, arr),0)];
-	arr[arr.indexOf(Math.min.apply(null, arr),0)] = temp;
-	console.log(arr);
+	let swapArr = [];
+	for(num of arr){
+		swapArr.push(num);
+	}
+	let max = Math.max.apply(null, swapArr);
+	let min = Math.min.apply(null, swapArr);
+	let indexMax = swapArr.indexOf(Math.max.apply(null, swapArr));
+	let indexMin = swapArr.indexOf(Math.min.apply(null, swapArr));
+	swapArr[indexMax] = min;
+	swapArr[indexMin] = max;
+	console.log(swapArr);
 }
 
 //second task
 function showMessage(text, callback){
 	console.log(`its just a function with ${text}`);
-	callback(); 
+	callback();
 }
 
 function showAnotherMessage(){
@@ -32,17 +41,23 @@ function showAnotherMessage(){
 }
 
 //third task
-function divNum(arr){
-	for (let i = 0; i <= arr.length; i++) {
-		if (arr[i]%3===0 && arr[i]%7===0){
-			arr[i] = 'foobar';
-		}else if (arr[i]%7===0){
-			arr[i] = 'bar';
-		} else if (arr[i]%3===0){
-			arr[i] = 'foo';
+function divNum(arr) {
+	let newArr = [];
+	arr.forEach(function(elem){
+		if(elem%3===0 && elem%7===0){
+			elem = 'foobar';
+			newArr.push(elem);
+		} else if(elem%3===0){
+			elem = 'foo';
+			newArr.push(elem);
+		} else if(elem%7===0){
+			elem = 'bar';
+			newArr.push(elem);
+		} else {
+			newArr.push(elem);
 		}
-	}
-	console.log(arr);
+	});
+	console.log(newArr);
 }
 
 //fourth task
@@ -50,7 +65,7 @@ function checkDiff(f,s) {
     let t = 0;
     for (let i = 0; i < f.length; i++) {
         for (let j = 0; j < s.length; j++) {
-            if (f[i] === s[j]) {
+            if (f[i]===s[j]) {
                 t++;
             }
         }
@@ -65,12 +80,8 @@ let arrayExample = {
 	name: 'John',
 	job: 'FE dev',
 	sport: 'Ultimate',
-	activeLife: true,
+	alife: true,
 	sayHi: function() {
 		console.log(`Hello, ${this.name}!`)
 	}
 };
-
-arrayExample.name = 'Alex';
-console.log(arrayExample);
-arrayExample.sayHi();
